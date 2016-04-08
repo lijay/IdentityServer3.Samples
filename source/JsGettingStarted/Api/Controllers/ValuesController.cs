387df2c1c7fe63c3e96e206a03187df4f7e25e96
lijaychen <lijaychen@moneydj.com>
2016-04-08 17:25:24 +0800

    Add JsGettingStarted sample

---
+++ source/JsGettingStarted/Api/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Api.Controllers
+{
+    [Route("values")]
+    public class ValuesController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            var random = new Random();
+
+            return new[]
+            {
+            random.Next(0, 10).ToString(),
+            random.Next(0, 10).ToString()
+            };
+        }
+    }
+}
---
+++ source/JsGettingStarted/IdentityServer/Code/Clients.cs
@@ -0,0 +1,44 @@
+﻿using IdentityServer3.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IdentityServer.Code
+{
+    public class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    Enabled = true,
+                    ClientName = "JS Client",
+                    ClientId = "js",
+                    Flow = Flows.Implicit,
+
+                    RedirectUris = new List<string>
+                    {
+                        "https://localhost:44301/popup.html",
+                        "https://localhost:44301/silent-renew.html"
+                    },
+
+                    PostLogoutRedirectUris = new List<string>()
+                    {
+                        "https://localhost:44301/index.html"
+                    },
+
+                    AllowedCorsOrigins = new List<string>
+                    {
+                        "https://localhost:44301"
+                    },
+
+                    AllowAccessToAllScopes = true,
+                    AccessTokenLifetime = 70
+                }
+            };
+        }
+    }
+}
